e4a25bcd4a5f7ee4254477bc35d7f49fd0bd7413
Aslan <tynyshtykaslan06@gmail.com>
2026-09-14 22:06:55 +0500

    feat(test-support): init

---
+++ test-support/src/main/java/com/travel/testsupport/TestSupportApplication.java
@@ -0,0 +1,13 @@
+package com.travel.testsupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSupportApplication.class, args);
+    }
+
+}
---
+++ test-support/src/test/java/com/travel/testsupport/TestSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.testsupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSupportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
